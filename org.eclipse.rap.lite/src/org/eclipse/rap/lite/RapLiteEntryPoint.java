ea601c717bf2dc46711ebbf224bbad9436ecc8e5
Tim Buschtöns <tbuschto@eclipsesource.com>
2013-02-07 23:04:45 +0100

    Add different buttons to demo (unsupported)

--- org.eclipse.rap.lite/src/org/eclipse/rap/lite/RapLiteEntryPoint.java
+++ org.eclipse.rap.lite/src/org/eclipse/rap/lite/RapLiteEntryPoint.java
@@ -24,13 +24,55 @@ public class RapLiteEntryPoint extends AbstractEntryPoint {
     label.setText( "Using " + RWT.getClient().getClass().getCanonicalName() );
     label.setLayoutData( new GridData( SWT.FILL, SWT.TOP, true, false ) );
     Button button = new Button( parent, SWT.PUSH );
+    button.setLayoutData( new GridData( SWT.LEFT, SWT.TOP, true, false ) );
     button.setText( "This is a button" );
     button.setImage( Display.getCurrent().getSystemImage( SWT.ICON_INFORMATION ) );
     button.addListener( SWT.Selection, new Listener() {
       @Override
       public void handleEvent( Event event ) {
         nr++;
-        label.setText( "Event " + nr );
+        label.setText( "Push " + nr );
+      }
+    } );
+    final Button check = new Button( parent, SWT.CHECK );
+    check.setLayoutData( new GridData( SWT.LEFT, SWT.TOP, true, false ) );
+    check.setText( "This is a Check Box" );
+    check.setImage( Display.getCurrent().getSystemImage( SWT.ICON_WARNING ) );
+    check.addListener( SWT.Selection, new Listener() {
+      @Override
+      public void handleEvent( Event event ) {
+        label.setText( "Check Box " + ( check.getSelection() ? "" : "not " ) + "Checked" );
+      }
+    } );
+    final Button toggle = new Button( parent, SWT.TOGGLE );
+    toggle.setLayoutData( new GridData( SWT.LEFT, SWT.TOP, true, false ) );
+    toggle.setText( "This is a toggle Button" );
+    toggle.addListener( SWT.Selection, new Listener() {
+      @Override
+      public void handleEvent( Event event ) {
+        label.setText( "Toggle Button " + ( toggle.getSelection() ? "Toggled" : "Untoggled (?)" ) );
+      }
+    } );
+    final Button radioA = new Button( parent, SWT.RADIO );
+    radioA.setLayoutData( new GridData( SWT.LEFT, SWT.TOP, true, false ) );
+    radioA.setText( "Radio A" );
+    radioA.addListener( SWT.Selection, new Listener() {
+      @Override
+      public void handleEvent( Event event ) {
+        if( radioA.getSelection() ) {
+          label.setText( "Option A" );
+        }
+      }
+    } );
+    final Button radioB = new Button( parent, SWT.RADIO );
+    radioB.setLayoutData( new GridData( SWT.LEFT, SWT.TOP, true, false ) );
+    radioB.setText( "Radio B" );
+    radioB.addListener( SWT.Selection, new Listener() {
+      @Override
+      public void handleEvent( Event event ) {
+        if( radioB.getSelection() ) {
+          label.setText( "Option B" );
+        }
       }
     } );
     getShell().addControlListener( new ControlListener() {
